7f241e509f808ce6308ab746af9ac2e9e49f67a5
Krzysztof Duda <duda_krzysztof@outlook.com>
2024-07-16 11:11:57 +0200

    Expand tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -12,12 +12,18 @@ import * as main from '../src/main'
 const runMock = jest.spyOn(main, 'run')
 
 let getInputMock: jest.SpiedFunction<typeof core.getInput>
+let errorMock: jest.SpiedFunction<typeof core.error>
+let setFailedMock: jest.SpiedFunction<typeof core.setFailed>
+let infoMock: jest.SpiedFunction<typeof core.info>
 
 describe('action', () => {
   beforeEach(() => {
     jest.clearAllMocks()
 
     getInputMock = jest.spyOn(core, 'getInput').mockImplementation()
+    errorMock = jest.spyOn(core, 'error').mockImplementation()
+    setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation()
+    infoMock = jest.spyOn(core, 'info').mockImplementation()
   })
 
   it('gets the correct path', async () => {
@@ -26,7 +32,7 @@ describe('action', () => {
         case 'main_translation_path':
           return './src/locales/en.json'
         case 'translation_paths':
-          return './src/locales/*.json'
+          return './src/locales/pl.json'
         default:
           return ''
       }
@@ -38,5 +44,55 @@ describe('action', () => {
 
     expect(getInputMock).toHaveBeenNthCalledWith(1, 'main_translation_path')
     expect(getInputMock).toHaveBeenNthCalledWith(2, 'translation_paths')
+  })
+
+  it('displays an no missing translations message', async () => {
+    getInputMock.mockImplementation(name => {
+      switch (name) {
+        case 'main_translation_path':
+          return 'example-translations/en.json'
+        case 'translation_paths':
+          return 'example-translations/pl.json'
+        default:
+          return ''
+      }
+    })
+
+    await main.run()
+
+    expect(runMock).toHaveReturned()
+
+    expect(infoMock).toHaveBeenCalledTimes(2)
+    expect(infoMock).toHaveBeenNthCalledWith(1, 'Checking translations...')
+    expect(infoMock).toHaveBeenNthCalledWith(2, 'No translations missing.')
+    expect(errorMock).not.toHaveBeenCalled()
+    expect(setFailedMock).not.toHaveBeenCalled()
+  })
+
+  it('displays an error message for missing translations', async () => {
+    getInputMock.mockImplementation(name => {
+      switch (name) {
+        case 'main_translation_path':
+          return 'example-translations/en.json'
+        case 'translation_paths':
+          return 'example-translations/pl-missing.json'
+        default:
+          return ''
+      }
+    })
+
+    await main.run()
+
+    expect(runMock).toHaveReturned()
+
+    expect(infoMock).toHaveBeenCalledTimes(1)
+    expect(infoMock).toHaveBeenNthCalledWith(1, 'Checking translations...')
+    expect(errorMock).toHaveBeenCalledTimes(1)
+    expect(errorMock).toHaveBeenCalledWith(
+      `Missing translation for key - 'index.description' for file 'example-translations/pl-missing.json'.`
+    )
+    expect(setFailedMock).toHaveBeenCalledWith(
+      'Action failed because translations are missing.'
+    )
   })
 })
